42665079a2645a34cf0da5eaec6640ca30d97520
echoui <bilal.echoui@gmail.com>
2015-05-05 19:31:41 +0200

    page aide avancée

---
+++ Aide_final2.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Proxipotage</title>
+	<link rel = "stylesheet" href="Aide_2.css" />
+    </head>
+
+    <body>
+
+
+<!--/////////////////////////////HEADER////////////
+///////////////////////////////////////////////////-->
+		<header class = "header">
+
+			<a href="index.html" >
+				<img src="images/pp_blanc.png" class="header-logo"/>
+			</a>
+				<nav class="nav-header">
+					<a href="#" class="menu-item">Forum</a>
+					<a href="#" class="menu-item">Déposer une annonce</a>
+					<a href="Lesoffres.html" class="menu-item">Les offres</a>
+					<a href="#" class="menu-item">Près de chez vous</a>
+					<a href="index.html" class="menu-item">Accueil</a>
+							
+					<form class="barre-recherchetxt" method="post" action="traitement.php">
+					   <p>
+					 	<label for ="recherche">
+						   		<!--<img src="images/loupe.png" <class="image-loupe">-->
+						</label> <input type="text" name="recherche" id="recherche" placeholder="Recherche" />
+					   </p>
+					</form>
+				</nav>
+				
+		</header>
+
+        <div class = "wrap">	
+			     
+                 <h1>Foire aux questions</h1>
+			     
+            
+        
+
+		<div class="Thematiques">
+				<ul>
+					<li class="theme">Déposer une offre</li>
+						<ol>
+                                                    <li><a href="#deposer_une_offre--1">Comment publier une offre ?</a></li>
+							<li><a href="#deposer_une_offre--2">Combien de temps mon offre reste-t-elle en ligne ?</a></li>
+							<li><a href="#deposer_une_offre--3">Que mettre dans le texte de mon offre ?</a></li>
+							<li><a href="#deposer_une_offre--4">Dois-je faire figurer mon email dans le texte de mon offre ?</a></li>
+							<li><a href="#deposer_une_offre--5">Comment confirmer mon offre ?</a></li>
+							<li><a href="#deposer_une_offre--6">Que faire si je ne reçois pas l'email de validation ?</a></li>
+						</ol>
+					<li class="theme">Gérer mes offres</li>
+						<ol>
+							<li><a href="#gerer_mes_offres--1">Comment retrouver mon offre ?</a></li>
+							<li><a href="#gerer_mes_offres--2">Comment modifier mon offre ?</a></li>
+							<li><a href="#gerer_mes_offres--3">Comment supprimer mon offre ?</a></li>
+							<li><a href="#gerer_mes_offres--4">Comment remonter mon offre en tête de liste ?</a></li>
+							<li><a href="#gerer_mes_offres--5">Que faire si j'ai oublié mon mot de passe ?</a></li>
+						</ol>
+					
+					<li class="theme">Consulter des offres</li>
+						<ol>
+							<li><a href="#consulter_des_offres--1">Comment chercher une offre ?</a></li>
+							<li><a href="#consulter_des_offres--2">Comment effectuer une recherche par mots-clés ?</a></li>
+							<li><a href="#consulter_des_offres--3">Comment répondre à une offre par email ?</a></li>
+							<li><a href="#consulter_des_offres--4">Où puis-je consulter les offres que j'ai sauvegardées ?</a></li>
+							<li><a href="#consulter_des_offres--5">Qu'est-ce que l'onglet "Près de chez vous" ?</a></li>
+								
+						</ol>
+					<li class="theme">Sécurité sur le site</li>
+						<ol>
+							<li><a href="#securite_sur_le_site--1">Une offre me paraît douteuse</a></li>
+							<li><a href="#securite_sur_le_site--2">J'ai reçu un email douteux en réponse à mon offre</a></li>
+							<li><a href="#securite_sur_le_site--3">Usurpation de mes coordonnées</a></li>
+							<li><a href="#securite_sur_le_site--4">ProxiPotage peut-il se porter garant des transactions effectuées sur le site ?</a></li>
+						</ol>
+					<li class="theme">Gérer mon compte personnel</li>
+						<ol>
+							<li><a href="#gerer_mon_compte_personnel--1">Créer mon compte</a></li>
+							<li><a href="#gerer_mon_compte_personnel--2">Mes données personnelles</a></li>
+							<li><a href="#gerer_mon_compte_personnel--3">Mes achats</a></li>
+							<li><a href="#gerer_mon_compte_personnel--4">Entrer dans mon compte</a></li>
+							<li><a href="#gerer_mon_compte_personnel--5">Mon profil</a></li>
+						</ol>
+				</ul>
+		</div>
+
+                <div>
+                    <h2>Déposer une offre</h2>
+                    
+                        <dl>
+                            <dt id="deposer_une_offre--1">Comment publier une offre ?</dt>
+                            <dd>Pour déposer une offre, le membre inscrit cliquera sur un onglet “déposer une annonce“.
+                                Il pourra alors créer une annonce, en choisissant par exemple si son offre sera disponible
+                                sous forme d’achat, d’échange, ou les deux.
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="deposer_une_offre--2">Combien de temps mon offre reste-t-elle en ligne ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="deposer_une_offre--3">Que mettre dans le texte de mon offre ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="deposer_une_offre--4">Dois-je faire figurer mon email dans le texte de mon offre ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="deposer_une_offre--5">Comment confirmer mon offre ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="deposer_une_offre--6">Que faire si je ne reçois pas l'email de validation ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                    <h2>Gérer mes offres</h2>
+                    
+                        <dl>
+                            <dt id="gerer_mes_offres--1">Comment retrouver mon offre ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="gerer_mes_offres--2">Comment modifier mon offre ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="gerer_mes_offres--3">Comment supprimer mon offre ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="gerer_mes_offres--4">Comment remonter mon offre en tête de liste ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="gerer_mes_offres--5">Que faire si j'ai oublié mon mot de passe ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                    <h2>Consulter des offres</h2>
+                    
+                        <dl>
+                            <dt id="consulter_des_offres--1">Comment chercher une offre ?</dt>
+                            <dd>Nous distinguerons donc deux types de recherche : textuelle et avancée.
+				La recherche textuelle consisterait à définir un seul bloc de recherche où on taperait le nom d’un fruit, légume, producteur ou d’un lieu.
+                                La recherche avancée aura plusieurs sections: localités, fruits, légumes, producteur, échange ou achat, pour permettre à l’utilisateur de filtrer ça recherche.L’utilisateur pourra visualiser les stocks d’une denrée chez un vendeur précis en sélectionnant un producteur dans la recherche avancée.
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="consulter_des_offres--2">Comment effectuer une recherche par mots-clés ?</dt>
+                            <dd>La page d’accueil permettra d’effectuer une recherche spécifique à l’intérieur d’une barre de recherche textuelle.<br />
+                                La recherche textuelle consisterait à définir un seul bloc de recherche où on taperait le nom d’un fruit, légume, producteur ou d’un lieu.
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="consulter_des_offres--3">Comment répondre à une offre par email ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="consulter_des_offres--4">Où puis-je consulter les offres que j'ai sauvegardées ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="consulter_des_offres--5">Qu'est-ce que l'onglet "Près de chez vous" ?</dt>
+                            <dd>Cet onglet, si l’on n’est pas inscrit, offre la possibilité de renseigner son adresse pour voir les offres près de chez sois.<br />
+                                Dans le cas où l’on est inscrit ET connecté cela se fait automatiquement avec l’adresse ou localité rentré en renseignement lors de l’inscription.
+                            </dd>
+                        </dl>
+                    
+                    <h2>Sécurité sur le site</h2>
+                    
+                        <dl>
+                            <dt id="securite_sur_le_site--1">Une offre me paraît douteuse</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="securite_sur_le_site--2">J'ai reçu un email douteux en réponse à mon offre</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="securite_sur_le_site--3">Usurpation de mes coordonnées</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="securite_sur_le_site--4">ProxiPotage peut-il se porter garant des transactions effectuées sur le site ?</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                    <h2>Gérer mon compte personnel</h2>
+                    
+                        <dl>
+                            <dt id="gerer_mon_compte_personnel--1">Créer mon compte</dt>
+                            <dd>Il sera demandé pour l’inscription de remplir des informations personnelles comme le nom, prénom, date de naissance, pseudo, adresse mail, téléphone et une adresse de vente. Il devra choisir un mot de passe et le confirmer. Il devra de plus renseigner sa localisation en désignant sa région, ville et code postal.Il y aura éventuellement la possibilité d’ajouter plusieurs adresses afin de proposer les différentes adresses lors de la vente.
+				L’inscription sera finalisée par l’acceptation des conditions générales du site puis enregistrée par le clic d’un bouton « valider ».
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="gerer_mon_compte_personnel--2">Mes données personnelles</dt>
+                            <dd>Il est possible de : Changer les informations liées à son compte (Tout est modifiable, coordonnées, e-mail et mot de passe),
+    				Supprimer son compte personnel (Attention si le compte est supprimé, cela supprime également les offres liées au compte. Cette opération est irréversible. 
+                                Un e-mail de validation de suppression de compte est envoyé. Si le lien présent dans cet e-mail n'est pas cliqué. Le compte n'est alors pas supprimé) et Compléter son profil
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="gerer_mon_compte_personnel--3">Mes achats</dt>
+                            <dd>Pour confirmer un achat, vous devez cliquer sur le bouton “Acheter“ puis vous serez redirigé vers une page vous permettant de renseigner, si possible, la quantité de produit voulu, et éventuellement un commentaire adressé au vendeur. 
+                                Par la suite vous serez amené à valider votre commande. Dans le cas où vous n'êtes pas connecté ou pas inscrit sur le site, vous serez invité à le faire pour pouvoir continuer l’achat.
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="gerer_mon_compte_personnel--4">Entrer dans mon compte</dt>
+                            <dd>L'identification au compte peut se faire depuis la zone d'authentification en haut à droite de chaque page ou en rentrant votre mot de passe de compte sur la second page du formulaire de dépôt d'offre. 
+                                Une fois authentifiée (e), vous avez la possibilité de vous déconnecter de votre compte toujours dans ce même espace en haut à droite.
+                            </dd>
+                        </dl>
+                    
+                        <dl>
+                            <dt id="gerer_mon_compte_personnel--5">Mon profil</dt>
+                            <dd>
+                            </dd>
+                        </dl>
+                    
+                        
+                    
+                </div>
+
+                    
+                        
+                    
+            </div>            
+                            
+                    
+	
+    </body>
+        
+</html>
+
+
+
+	
+
